e4e4774e9fdfc2b254058a8433ed3c1e2f4a0c19
reyev <alexander.rayev@infineon.com>
2025-06-03 17:05:56 -0400

    Updated stack

--- infrastructure/utils/pipelineConfig.ts
+++ infrastructure/utils/pipelineConfig.ts
@@ -6,7 +6,7 @@ export const pipelineConfig = (env: string) => {
 
     return {
       buildCommand: 'yarn build:prod',
-      deployCommand: 'cdk deploy',
+      deployCommand: 'yarn cdk deploy',
       branch: 'main',
       tag: 'chapter5-production-pipeline',
       githubToken: parsed?.GITHUB_TOKEN,
@@ -19,7 +19,7 @@ export const pipelineConfig = (env: string) => {
 
   return {
     buildCommand: 'yarn build:dev',
-    deployCommand: 'cdk:dev deploy',
+    deployCommand: 'yarn cdk:dev deploy',
     branch: 'dev',
     tag: 'chapter5-development-pipeline',
     githubToken: parsed?.GITHUB_TOKEN,
